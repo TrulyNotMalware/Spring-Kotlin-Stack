c26a5df13f31c324cd31e8d9a06dab696d9668f1
TrulyNotMalware <notleebutyee@gmail.com>
2024-01-16 17:30:01 +0900

    feat : OAuth application convert
    - convert dao
    - convert domains
    no related issue

--- oauthServer/src/main/kotlin/dev/notypie/application/DefaultOAuth2ServiceImpl.kt
+++ oauthServer/src/main/kotlin/dev/notypie/application/DefaultOAuth2ServiceImpl.kt
@@ -1,17 +1,21 @@
 package dev.notypie.application
 
+import dev.notypie.domain.Client
 import dev.notypie.dto.RegisterOAuthClient
 import dev.notypie.dto.ResponseRegisteredClient
+import dev.notypie.jpa.dao.ClientRepository
+import dev.notypie.jwt.utils.logger
 import org.springframework.stereotype.Service
 
 @Service
 class DefaultOAuth2ServiceImpl(
-//    private val clientRepository: ClientRepository
+    private val clientRepository: ClientRepository
 ) : OAuth2Service{
 
+    private val log = logger()
 
-    override fun registerNewClient(oAuthClient: RegisterOAuthClient): ResponseRegisteredClient {
-        TODO("Not yet implemented")
-    }
+    override fun registerNewClient(oAuthClient: RegisterOAuthClient): ResponseRegisteredClient
+    = this.clientRepository.save(Client.createDefaultClient(oAuthClient)).toResponseDto()
+
 
 }
